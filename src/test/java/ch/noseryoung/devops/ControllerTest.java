1f20c7066fd9880ba0246efcc0d78df636658238
flxsch <felix.schneider@edu.tbz.ch>
2019-11-20 16:33:39 +0100

    second commit

---
+++ src/test/java/ch/noseryoung/devops/ControllerTest.java
@@ -0,0 +1,12 @@
+package ch.noseryoung.devops;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ControllerTest {
+
+  @Test
+  void helloWorld() {
+    Assertions.assertThat(1).isEqualTo(1);
+  }
+}
