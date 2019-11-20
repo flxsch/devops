14d6099c0b16c3a911750399cb750e0303f0dec2
flxsch <felix.schneider@edu.tbz.ch>
2019-11-20 16:20:34 +0100

    first commit

---
+++ src/test/java/ch/noseryoung/devops/DevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.noseryoung.devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
