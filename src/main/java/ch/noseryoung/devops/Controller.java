ef1f92d774f00880e28951bf6a7d533b6d700bf4
flxsch <felix.schneider@edu.tbz.ch>
2019-11-21 16:09:13 +0100

    added endpoint

--- src/main/java/ch/noseryoung/devops/Controller.java
+++ src/main/java/ch/noseryoung/devops/Controller.java
@@ -3,9 +3,11 @@ package ch.noseryoung.devops;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/hello")
 public class Controller {
 
     @GetMapping
