bdb6553f128951eaca6975c02303a6d8a267015a
flxsch <felix.schneider@edu.tbz.ch>
2019-11-21 15:33:52 +0100

    docker added

--- src/main/java/ch/noseryoung/devops/DevopsApplication.java
+++ src/main/java/ch/noseryoung/devops/DevopsApplication.java
@@ -2,6 +2,7 @@ package ch.noseryoung.devops;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class DevopsApplication {
